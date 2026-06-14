69d7e8e9093c7672acd28e11c9b5bbdc19102eb5
Aka_Manto <khushidimri601@gmail.com>
2026-06-14 09:08:53 +0530

    Files got added

---
+++ .ipynb_checkpoints/demo-checkpoint.py
@@ -0,0 +1,13 @@
+import sklearn
+import numpy as np
+import pandas as pd
+import sys 
+import scipy
+import matplotlib
+#iris = datasets.load_iris()
+print("Scikit-learn version: {}".format(sklearn.__version__))
+print("Panda version : {}".format(pd.__version__))
+print("Numpy version : {}".format(np.__version__))
+print("python version : {}".format(sys.version))
+print("Scipy version : {}".format(scipy.__version__))
+print("Matplotlib version : {}".format(matplotlib.__version__))
